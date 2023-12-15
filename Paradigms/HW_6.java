430cad39719b8420c54becbb8e16648e99435c4b
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-12-15 23:43:06 +0500

    Update HW_6.java

--- Paradigms/HW_6.java
+++ Paradigms/HW_6.java
@@ -1,1 +1,36 @@
+public class HW_6 {
 
+    public static void main(String[] args) {
+        int[]arrayNumbers = new int[]{1,2,3,4,5,6,7,8,9};
+        System.out.println(binary_search(arrayNumbers, 1));
+
+
+    }
+
+    public static int binary_search(int[]arrayNumbers, int searchNumber){
+        int minValue = 0;
+        int maxValue = arrayNumbers.length - 1;
+
+
+        while (minValue <= maxValue){
+            int mid = (minValue + maxValue)/2;
+            int midIndexValue = arrayNumbers[mid];
+
+            if (midIndexValue == searchNumber){
+                return mid;
+            }
+            if (midIndexValue > searchNumber) {
+                maxValue = mid - 1;
+
+            }
+            if (midIndexValue < searchNumber){
+
+                minValue = mid + 1;
+            }
+
+
+        }
+        return -1;
+
+    }
+}
